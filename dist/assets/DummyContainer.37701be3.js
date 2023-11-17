be2ac1e01eb95148bf0df5327f9c4d1c9772fefd
wby <wby@ojbk.io>
2023-11-17 14:38:07 +0800

    frontend fixed

--- dist/assets/DummyContainer.37701be3.js
+++
@@ -1,1 +0,0 @@
-import{cE as o,m as r,bp as n,l as t}from"./index.a143a9e8.js";const c={};function a(s,_){const e=n("router-view");return t(),r(e)}var p=o(c,[["render",a]]);export{p as default};
---
+++ dist/assets/DummyContainer.53b01529.js
@@ -0,0 +1,1 @@
+import{cE as o,m as r,bp as n,l as t}from"./index.5a9c8099.js";const c={};function a(s,_){const e=n("router-view");return t(),r(e)}var p=o(c,[["render",a]]);export{p as default};
