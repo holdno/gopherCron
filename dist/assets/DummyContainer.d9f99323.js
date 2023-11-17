0da49ba3b4ded4a3449df4458f7fdc92dff982ff
wby <wby@ojbk.io>
2023-11-17 15:39:48 +0800

    upgrade frontend

---
+++ dist/assets/DummyContainer.d9f99323.js
@@ -0,0 +1,1 @@
+import{cE as o,m as r,bp as n,l as t}from"./index.5d971b20.js";const c={};function a(s,_){const e=n("router-view");return t(),r(e)}var p=o(c,[["render",a]]);export{p as default};
---
+++ dist/assets/QSpace.8042c2a8.js
@@ -0,0 +1,1 @@
+import{h as a,a as e}from"./index.5d971b20.js";const p=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>p}});export{c as Q};
