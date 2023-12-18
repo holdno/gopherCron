845fb0c952e88b52220beaa60ae4f3dd994e7e92
boyec <w@ojbk.io>
2023-12-18 14:49:52 +0800

    improve oidc login redirect

--- dist/assets/QSpace.beef6455.js
+++
@@ -1,1 +0,0 @@
-import{h as a,a as e}from"./index.7a92b1b1.js";const p=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>p}});export{c as Q};
--- dist/assets/use-quasar.8072e87c.js
+++
@@ -1,1 +0,0 @@
-import{a4 as a,aD as r}from"./index.7a92b1b1.js";function u(){return a(r)}export{u};
