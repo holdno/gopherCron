dcfd2958fb7386d946ee16e398229076c06118d6
boyec <w@ojbk.io>
2024-01-24 18:51:10 +0800

    modify agent weight & tmp task host

--- agent/watcher.go
+++ agent/watcher.go
@@ -38,8 +38,10 @@ func (a *client) handlerEventFromCenterV2(ctx context.Context, event *cronpb.Ser
 	replyEvent := newReplyEvent(event.Id)
 	switch event.Type {
 	case cronpb.EventType_EVENT_REGISTER_HEARTBEAT_PING:
+		// 建连后日常ping，某些基础设施需要显示的调用来保持链接的活跃(避免被强制关闭)
 		replyEvent.Type = cronpb.EventType_EVENT_REGISTER_HEARTBEAT_PONG
 	case cronpb.EventType_EVENT_REGISTER_REPLY:
+		// agent注册后，中心会逐步下发需要该agent运行的任务信息
 		data := event.GetRegisterReply()
 		if len(data.Value) != 0 { // 过滤脏数据
 			if err := a.handlerEventFromCenter(data); err != nil {
@@ -47,6 +49,7 @@ func (a *client) handlerEventFromCenterV2(ctx context.Context, event *cronpb.Ser
 			}
 		}
 	case cronpb.EventType_EVENT_SCHEDULE_REQUEST:
+		// 立即调度一个任务
 		replyEvent.Type = cronpb.EventType_EVENT_SCHEDULE_REPLY
 		resp, err := a.Schedule(ctx, event.GetScheduleRequest())
 		if err != nil {
@@ -57,6 +60,7 @@ func (a *client) handlerEventFromCenterV2(ctx context.Context, event *cronpb.Ser
 			}
 		}
 	case cronpb.EventType_EVENT_KILL_TASK_REQUEST:
+		// 立即结束某个运行中的任务
 		replyEvent.Type = cronpb.EventType_EVENT_KILL_TASK_REPLY
 		resp, err := a.KillTask(ctx, event.GetKillTaskRequest())
 		if err != nil {
@@ -67,6 +71,7 @@ func (a *client) handlerEventFromCenterV2(ctx context.Context, event *cronpb.Ser
 			}
 		}
 	case cronpb.EventType_EVENT_PROJECT_TASK_HASH_REQUEST:
+		// 获取边缘某个项目下所有任务计算出的hash值，用于比较不同agent间任务状态是否存在差异
 		replyEvent.Type = cronpb.EventType_EVENT_PROJECT_TASK_HASH_REPLY
 		resp, err := a.ProjectTaskHash(ctx, event.GetProjectTaskHashRequest())
 		if err != nil {
@@ -77,6 +82,7 @@ func (a *client) handlerEventFromCenterV2(ctx context.Context, event *cronpb.Ser
 			}
 		}
 	case cronpb.EventType_EVENT_COMMAND_REQUEST:
+		// 中心下发一些agent可以执行的指令
 		replyEvent.Type = cronpb.EventType_EVENT_COMMAND_REPLY
 		resp, err := a.Command(ctx, event.GetCommandRequest())
 		if err != nil {
@@ -87,6 +93,7 @@ func (a *client) handlerEventFromCenterV2(ctx context.Context, event *cronpb.Ser
 			}
 		}
 	case cronpb.EventType_EVENT_CHECK_RUNNING_REQUEST:
+		// 中心询问agent是否正在运行某个任务
 		replyEvent.Type = cronpb.EventType_EVENT_CHECK_RUNNING_REPLY
 		resp, err := a.CheckRunning(ctx, event.GetCheckRunningRequest())
 		if err != nil {
@@ -95,6 +102,15 @@ func (a *client) handlerEventFromCenterV2(ctx context.Context, event *cronpb.Ser
 			replyEvent.Event = &cronpb.ClientEvent_CheckRunningReply{
 				CheckRunningReply: resp,
 			}
+		}
+	case cronpb.EventType_EVENT_MODIFY_NODE_META:
+		// 中心直接下发指令变更agent的权重
+		a.cfg.Micro.Weight = event.GetModifyNodeMeta().Weight
+		replyEvent.Event = &cronpb.ClientEvent_ModifyNodeMeta{
+			ModifyNodeMeta: &cronpb.Result{
+				Result:  true,
+				Message: "ok",
+			},
 		}
 	default:
 		return &cronpb.ClientEvent{
