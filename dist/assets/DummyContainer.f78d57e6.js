fe3422f592f87058d9ca17afc14e06859b897914
boyce <w@ojbk.io>
2024-05-15 17:27:14 +0800

    fix frontend workflow task detail

---
+++ dist/assets/DummyContainer.f78d57e6.js
@@ -0,0 +1,1 @@
+import{_ as o,ag as r,c as n,o as t}from"./index.b313b377.js";const c={};function s(a,_){const e=r("router-view");return t(),n(e)}var u=o(c,[["render",s],["__file","/Users/boyan/development/opensource/gopherCronFE/src/layouts/DummyContainer.vue"]]);export{u as default};
