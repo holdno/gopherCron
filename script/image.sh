801eaa4ef3bdf004b1be9d45e18fddd75e7239eb
boyce <w@ojbk.io>
2024-05-15 15:14:03 +0800

    modify docker build script

--- script/image.sh
+++ script/image.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 IMAGE_PROJECT=holdnowby
 IMAGE_NAME=gophercron
+VERSION=$1
 
 # Make full image name
-IMAGE=${IMAGE_PROJECT}/${IMAGE_NAME}:v2.4.6
+IMAGE=${IMAGE_PROJECT}/${IMAGE_NAME}:${VERSION}
 
 docker build -t ${IMAGE} .
+
+docker push ${IMAGE_PROJECT}/${IMAGE_NAME}:${VERSION}
